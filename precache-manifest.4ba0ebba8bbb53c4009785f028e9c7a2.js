e6765e39d61f285abaa9a179558424a02c1ceea7
Suvorov Aleksey <suvorov@hostco.ru>
2020-05-31 23:58:20 +0300

    Updates

---
+++ precache-manifest.4ba0ebba8bbb53c4009785f028e9c7a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786db864b2e47ed8a445e59d30fca2e1",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "6303aff15dc7b4e09562",
+    "url": "/quiz-app/static/css/main.83bf4a7a.chunk.css"
+  },
+  {
+    "revision": "31eada8348aeaa590af0",
+    "url": "/quiz-app/static/js/2.68a8dc70.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.68a8dc70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6303aff15dc7b4e09562",
+    "url": "/quiz-app/static/js/main.1d870065.chunk.js"
+  },
+  {
+    "revision": "f0c20b32b7eb7118580f",
+    "url": "/quiz-app/static/js/runtime-main.6864632f.js"
+  }
+]);
